7fb9759ccc2a9793aad74de0a3bd733ed7a4e5c5
JadonXia <jadon.xia@ocsbstudent.ca>
2021-03-23 19:11:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// These are the variables I will need to calculate area and circumference.
+let d = 0
+let r = d / 2
+let ans1 = Math.PI * r ** 2
+let ans2 = 2 * Math.PI * r
+
+// This script will link my button to a function.
+document.getElementById('button1').addEventListener('click', calculate)
+
+// Next, we have to write the code for what the function 'calculate' does.
+function calculate () {
+  d = document.getElementById('text1').value
+  d = parseInt(d)
+  document.getElementById('results1').innerHTML = ans1
+  document.getElementById('results2').innerHTML = ans2
+}
